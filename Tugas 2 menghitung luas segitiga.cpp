c2f0eb36ff1226811cf64787381656896f56cb97
Akbar Bagus Setiawan <50337308+akbartv@users.noreply.github.com>
2022-01-14 13:51:36 +0700

    Add files via upload

---
+++ Tugas 2 menghitung luas segitiga.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<stdlib.h>
+
+using namespace std;
+
+int main() {
+	int a,t;
+	
+	float bagi=0.5,luas;
+	
+	cout<<"masukan alas: ";
+	cin>> a;
+	cout<<"masukan tinggi: ";
+	cin>> t;
+	luas=bagi*a*t;
+	cout<<"luas segit iga adalah"<<luas;
+	
+	return 0;
+	
+	
+}
