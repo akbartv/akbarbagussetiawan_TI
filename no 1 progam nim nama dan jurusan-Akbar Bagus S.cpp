ee2d9e64209d09bfacd8fe53db0e99000964f7a3
Akbar Bagus Setiawan <50337308+akbartv@users.noreply.github.com>
2022-01-16 14:32:08 +0700

    Add files via upload

---
+++ no 1 progam nim nama dan jurusan-Akbar Bagus S.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main (){
+	string nama;
+	string nim;
+	string jurusan;
+	
+	cout<<endl;
+	cout<<" PROGRAM DATA MAHASISWA"<<endl; 
+	cout<<"------------------------------"<<endl;
+	
+	cout<<" Nama    : Akbar Bagus Setiawan"<<nama<<endl;
+	cout<<" Nim     : 07.1.2021.0041"<<nim<<endl;
+	cout<<" Jurusan : Teknik Informatika"<<jurusan<<endl;
+
+}
